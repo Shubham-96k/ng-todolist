359ecbb634bfd8f7b7f158c3094af044dfb39716
Shubham-96k <iamcoder8989@gmail.com>
2024-01-31 00:33:01 +0530

    worked on ng-todo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myproject';
+  title = 'todolist';
+  skillsArray : Array<string> = ['Html 5', 'CSS 3', 'Javascript', 'Angular', 'Node']
+
+  // addtodo(todo: HTMLInputElement){
+  //   // console.log(todo);
+  //   if(todo.value){
+  //     console.log(todo.value);
+  //     this.skillsArray.push(todo.value);
+  //     todo.value = "";
+  //   }
+  // }
+
+  addtodoitem(todo : string){
+    console.log(todo);
+    this.skillsArray.push(todo);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoformComponent } from './components/todoform/todoform.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoformComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
